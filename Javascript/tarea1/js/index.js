e434c4251cde756482b88d72b7b4ef05ed8ba339
Francisco Agustin Munoz <franmunoz2501@gmail.com>
2023-11-24 10:21:45 -0300

    afterclass-1

---
+++ Javascript/tarea1/js/index.js
@@ -0,0 +1,47 @@
+//ej 1
+// let usuario = "fran"
+// let edad
+
+// usuario = prompt("Ingrese su usuario: ")
+// edad = parseInt(prompt("Ingrese su edad: "))
+
+// if ((usuario == "fran") && (edad >= 18)) {
+//     console.log("Bienvenido usuario")
+// } else {
+//     console.log("Acceso denegado")
+// }
+
+//ej 2
+// let producto;
+// let stock = true
+// let descuento = true
+
+// if ((stock == true) && (descuento == true)) {
+//     console.log("Producto en stock")
+// } else {
+//     console.log("Producto sin stock")
+// }
+
+// ej3
+// let usuario
+// let password
+// let inicio = false
+
+// if (inicio == true) {
+//     console.log("bienvenido usuario")
+// } else {
+//     console.log("debe iniciar sesion")
+// }
+
+//ej 4
+// let usuario
+// let password
+
+// usuario = prompt("Ingrese el usuario: ")
+// password = prompt("Ingrese la contraseña: ")
+
+// if ((usuario == "admin") && ((password == "secreta") || (password == "123456"))) {
+//     console.log("Bienvenido usuario!")
+// } else {
+//     console.log("Acceso denegado")
+// }
