84a89a3d641eeff355e465a6946409b28b800b4c
Francisco Agustin Munoz <franmunoz2501@gmail.com>
2023-12-07 13:47:02 -0300

    actividades corregidas

--- Javascript/clase6/actividades/js/index.js
+++ Javascript/clase6/actividades/js/index.js
@@ -9,13 +9,28 @@ const PERSONAS = [
 ];
 
 let sumatoria = 0;
+let resultado = 0;
 
-for (const personaRecorrido of PERSONAS) {
-    sumatoria += personaRecorrido.edad
+function calcularPromedio(personaRecorrido) {
+    for (const personaRecorrido of PERSONAS) {
+        sumatoria += personaRecorrido.edad
+    }
+
+    let resultado = (sumatoria) / (PERSONAS.length)
+
+    return resultado
 }
 
-let resultado = (sumatoria) / (PERSONAS.length)
-console.log("La edad promedio es: " + resultado)
+// console.log("La edad promedio es: " + calcularPromedio(PERSONAS))
+
+// for (const personaRecorrido of PERSONAS) {
+//     sumatoria += personaRecorrido.edad
+// }
+
+// let resultado = (sumatoria) / (PERSONAS.length)
+console.log("La edad promedio es: " + calcularPromedio(PERSONAS))
+
+// como hacerlo con una funcion
 
 // Actividad 2
 
@@ -23,16 +38,22 @@ const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let numerosPares = []
 let numerosImpares = []
 
-for (const recorridoNumeros of numeros) {
-    if (recorridoNumeros % 2 == 0) {
-        numerosPares.push(recorridoNumeros)
-    } else {
-        numerosImpares.push(recorridoNumeros)
+
+function calcularParidad(recorridoNumeros) {
+    for (const recorridoNumeros of numeros) {
+        if (recorridoNumeros % 2 == 0) {
+            numerosPares.push(recorridoNumeros)
+        } else {
+            numerosImpares.push(recorridoNumeros)
+        }
     }
+
+    return numerosPares
 }
 
-console.log("Los numeros pares son: " + numerosPares)
-console.log("Los numeros impares son: " + numerosImpares)
+
+console.log("Los numeros pares son: " + calcularParidad(numeros))
+
 
 // Actividad 3
 
@@ -40,32 +61,78 @@ let estudiantesAprobados = []
 
 const ESTUDIANTES = [
 
-    { nombre: "Francisco", nota: 80 },
-    { nombre: "Pedro", nota: 30 },
-    { nombre: "Carlos", nota: 55 },
-    { nombre: "Juan", nota: 76 },
-    { nombre: "Esteban", nota: 23 },
-    { nombre: "Alexandro", nota: 90 },
+    { nombre: "Francisco", calificacion: 80 },
+    { nombre: "Pedro", calificacion: 30 },
+    { nombre: "Carlos", calificacion: 55 },
+    { nombre: "Juan", calificacion: 76 },
+    { nombre: "Esteban", calificacion: 23 },
+    { nombre: "Alexandro", calificacion: 90 },
 
 ];
 
-for (const recorridoEstudiantes of ESTUDIANTES) {
-    if (recorridoEstudiantes.nota > 60) {
-        estudiantesAprobados.push(recorridoEstudiantes)
+
+function alumnosAprobados(recorridoEstudiantes) {
+    for (const recorridoEstudiantes of ESTUDIANTES) {
+        if (recorridoEstudiantes.calificacion >= 60) {
+            estudiantesAprobados.push(recorridoEstudiantes)
+        }
     }
+    return estudiantesAprobados
 }
 
-console.log(estudiantesAprobados)
+console.log(alumnosAprobados(ESTUDIANTES))
 
 //como hacer para poder ver el nombre solo de los estudiantes aprobados pero almacenando el vector completo
 
 // Actividad 4
 
-const PALABRAS = ["hola", "como", "estas", "hola"]
-let repeticion
+// const palabras = ["manzana", "banana", "manzana", "naranja", "banana", "frutilla"];
+// let conteo = [];
 
-for (let i = 0; i < PALABRAS.length; i++) {
-    repeticion = PALABRAS.indexOf("hola")
+// function contadorDePalabras(arrayPalabras) {
+
+//     for (let i = 0; i < arrayPalabras; i++) {
+
+//         const palabra = arrayPalabras[i]
+
+//         if (conteo[palabra]) {
+//             // si se repite incrementa en uno
+//             conteo[palabra]++;
+
+//         } else {
+//             // si no se repite se toma como uno
+//             conteo[palabra] = 1;
+//         }
+//     }
+
+//     return conteo;
+// }
+
+// const cuantasVeces = contadorDePalabras(palabras)
+
+// console.log(contadorDePalabras(palabras))
+
+// Ejemplo compañero
+
+const palabrasIngresadas = []
+
+for (i = 0; i < 5; i++) {
+    let palabra = prompt("ingrese su palabra");
+    palabrasIngresadas.push(palabra)
 }
 
-console.log(repeticion)
+
+function contarPalabras(arrays) {
+    const conteoPalabras = {};
+    arrays.forEach(palabra => {
+        if (conteoPalabras[palabra]) {
+            conteoPalabras[palabra]++
+        } else {
+            conteoPalabras[palabra] = 1
+        }
+    });
+    return conteoPalabras
+}
+
+const conteo = contarPalabras(palabrasIngresadas);
+console.log(conteo)
