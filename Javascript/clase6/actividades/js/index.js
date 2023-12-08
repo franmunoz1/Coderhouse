b9a729b3d49f4882c20ba1105f59c11e430f88fa
Francisco Agustin Munoz <franmunoz2501@gmail.com>
2023-12-08 10:06:10 -0300

    repaso de funciones flecha

--- Javascript/clase6/actividades/js/index.js
+++ Javascript/clase6/actividades/js/index.js
@@ -1,25 +1,25 @@
 // Actividad 1
 
-const PERSONAS = [
+// const PERSONAS = [
 
-    { nombre: "Juan", edad: 30 },
-    { nombre: "Maria", edad: 25 },
-    { nombre: "Luis", edad: 35 }
+//     { nombre: "Juan", edad: 30 },
+//     { nombre: "Maria", edad: 25 },
+//     { nombre: "Luis", edad: 35 }
 
-];
+// ];
 
-let sumatoria = 0;
-let resultado = 0;
+// let sumatoria = 0;
+// let resultado = 0;
 
-function calcularPromedio(personaRecorrido) {
-    for (const personaRecorrido of PERSONAS) {
-        sumatoria += personaRecorrido.edad
-    }
+// function calcularPromedio(personaRecorrido) {
+//     for (const personaRecorrido of PERSONAS) {
+//         sumatoria += personaRecorrido.edad
+//     }
 
-    let resultado = (sumatoria) / (PERSONAS.length)
+//     let resultado = (sumatoria) / (PERSONAS.length)
 
-    return resultado
-}
+//     return resultado
+// }
 
 // console.log("La edad promedio es: " + calcularPromedio(PERSONAS))
 
@@ -28,31 +28,31 @@ function calcularPromedio(personaRecorrido) {
 // }
 
 // let resultado = (sumatoria) / (PERSONAS.length)
-console.log("La edad promedio es: " + calcularPromedio(PERSONAS))
+// console.log("La edad promedio es: " + calcularPromedio(PERSONAS))
 
 // como hacerlo con una funcion
 
 // Actividad 2
 
-const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let numerosPares = []
-let numerosImpares = []
+// const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let numerosPares = []
+// let numerosImpares = []
 
 
-function calcularParidad(recorridoNumeros) {
-    for (const recorridoNumeros of numeros) {
-        if (recorridoNumeros % 2 == 0) {
-            numerosPares.push(recorridoNumeros)
-        } else {
-            numerosImpares.push(recorridoNumeros)
-        }
-    }
+// function calcularParidad(recorridoNumeros) {
+//     for (const recorridoNumeros of numeros) {
+//         if (recorridoNumeros % 2 == 0) {
+//             numerosPares.push(recorridoNumeros)
+//         } else {
+//             numerosImpares.push(recorridoNumeros)
+//         }
+//     }
 
-    return numerosPares
-}
+//     return numerosPares
+// }
 
 
-console.log("Los numeros pares son: " + calcularParidad(numeros))
+// console.log("Los numeros pares son: " + calcularParidad(numeros))
 
 
 // Actividad 3
@@ -71,7 +71,7 @@ const ESTUDIANTES = [
 ];
 
 
-function alumnosAprobados(recorridoEstudiantes) {
+let alumnosAprobados = (recorridoEstudiantes) => {
     for (const recorridoEstudiantes of ESTUDIANTES) {
         if (recorridoEstudiantes.calificacion >= 60) {
             estudiantesAprobados.push(recorridoEstudiantes)
@@ -114,25 +114,25 @@ console.log(alumnosAprobados(ESTUDIANTES))
 
 // Ejemplo compañero
 
-const palabrasIngresadas = []
+// const palabrasIngresadas = []
 
-for (i = 0; i < 5; i++) {
-    let palabra = prompt("ingrese su palabra");
-    palabrasIngresadas.push(palabra)
-}
+// for (i = 0; i < 5; i++) {
+//     let palabra = prompt("ingrese su palabra");
+//     palabrasIngresadas.push(palabra)
+// }
 
 
-function contarPalabras(arrays) {
-    const conteoPalabras = {};
-    arrays.forEach(palabra => {
-        if (conteoPalabras[palabra]) {
-            conteoPalabras[palabra]++
-        } else {
-            conteoPalabras[palabra] = 1
-        }
-    });
-    return conteoPalabras
-}
+// function contarPalabras(arrays) {
+//     const conteoPalabras = {};
+//     arrays.forEach(palabra => {
+//         if (conteoPalabras[palabra]) {
+//             conteoPalabras[palabra]++
+//         } else {
+//             conteoPalabras[palabra] = 1
+//         }
+//     });
+//     return conteoPalabras
+// }
 
-const conteo = contarPalabras(palabrasIngresadas);
-console.log(conteo)
+// const conteo = contarPalabras(palabrasIngresadas);
+// console.log(conteo)
