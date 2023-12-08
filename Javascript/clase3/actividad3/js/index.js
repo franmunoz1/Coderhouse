3c1f643628944e7b24bea76eae3551af953d1f9a
Francisco Agustin Munoz <franmunoz2501@gmail.com>
2023-12-07 23:26:36 -0300

    actividades con funciones

---
+++ Javascript/clase3/actividad3/js/index.js
@@ -0,0 +1,68 @@
+// Actividad 1
+
+// let resultado = 0
+
+// for (let i = 1; i <= 50; i++) {
+//     if (i % 2 == 0) {
+//         resultado += i;
+//     }
+// }
+
+// console.log("La sumatoria de todos los numeros pares es: " + resultado)
+
+
+// Actividad 2
+
+// let numero = 0
+// const NUMEROSECRETO = parseInt(prompt("Ingrese el numero secreto: "))
+
+// do {
+
+//     numero = parseInt(prompt("Ingrese un numero: "));
+
+//     if (numero > NUMEROSECRETO) {
+//         console.log("El numero es menor")
+//     } else if (numero < NUMEROSECRETO) {
+
+//         console.log("El numero es mayor")
+
+//     }
+
+// } while (numero != NUMEROSECRETO)
+
+// console.log("Adivinaste!")
+
+// Actividad 3
+
+// let condicion, opcion;
+
+// let numero1 = parseInt(prompt("Ingrese el primer numero: "));
+// let numero2 = parseInt(prompt("Ingrese el segundo numero: "));
+
+
+// do {
+
+//     opcion = prompt("Ingrese la operacion a realizar: ")
+
+//     switch (opcion) {
+//         case "suma":
+//             console.log("La suma da: " + (numero1 + numero2))
+//             break;
+//         case "resta":
+//             console.log("La resta da: " + (numero1 - numero2))
+//             break;
+//         case "multiplicacion":
+//             console.log("La multiplicacion da: " + (numero1 * numero2))
+//             break;
+//         case "division":
+//             console.log("La division da: " + (numero1 / numero2))
+//             break;
+//     }
+
+
+//     condicion = prompt("Desea realizar otra operacion si/no: ")
+
+// } while (condicion == "si")
+
+
+// Actividad 4
