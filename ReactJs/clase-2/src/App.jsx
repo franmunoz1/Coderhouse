16bdbf40745379a5e052b291d46850183fdbbeb2
Francisco Agustin Munoz <franmunoz2501@gmail.com>
2024-02-21 12:03:02 -0300

    clase 3 react

--- ReactJs/clase-2/src/App.jsx
+++ ReactJs/clase-2/src/App.jsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const estilos = {
+    width: '500px',
+    height: '500px',
+    borderRadius: '50%',
+    border: '5px solid black',
+    margin: '10px'
+  }
 
   return (
+
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <h1>Mi primer proyecto</h1>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className='titulo'>Hola mundo!</h1>
+      <img style={estilos} src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/151.png" alt="" />
     </>
   )
 }
